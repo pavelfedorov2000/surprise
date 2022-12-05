2a6dc9bf63ecaa244a9d85765debb8167dcc3878
Pavel Fedorov <p.fedorov@imedia.by>
2022-12-05 09:29:40 +0300

    certificate-page

--- app/blocks/aside-choose/aside-choose.js
+++ app/blocks/aside-choose/aside-choose.js
@@ -1,7 +1,22 @@
-app.yourScriptName = {
-  name: 'yourScriptName',
+app.asideChoose = {
+  name: 'asideChoose',
   description: 'your script description',
   init() {
-    // your code
+    // choose package
+
+    $('.js-choose-package').on('click', function () {
+      const $radioInput = $(this);
+      const $choosePackageItem = $radioInput.closest('.choose-package__item');
+      const $choosePackageDropdown = $choosePackageItem.find('.choose-package__dropdown');
+
+      const $choosePackageItems = $choosePackageItem.siblings();
+      const $radioInputNotThis = $choosePackageItems.find('input[type=radio');
+      const $choosePackageDropdownNotThis = $choosePackageItems.find('.choose-package__dropdown');
+
+      $radioInputNotThis.prop('checked', false);
+      $choosePackageDropdownNotThis.slideUp();
+
+      $choosePackageDropdown.slideDown();
+    });
   },
 };
